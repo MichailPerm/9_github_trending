e841880448ade38b81d79eb0e3d1e21936f647cc
Michail_Z <apxahrejl@gmail.com>
2018-05-27 22:25:42 +0500

    store changes

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,28 @@
+import requests
+import json
+from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+API_REPO_URL = 'https://api.github.com/search/repositories?q=created:<{}'
+API_ISSUES_URL = 'https://api.github.com/repos/{}/issues'
+
+def get_trending_repositories(date_week_earlier):
+    repo_quantity = 20
+    repo_data = requests.get(API_REPO_URL.format(date_week_earlier)).json()
+    repo_list = repo_data['items']
+    repo_list_sorted = sorted(repo_list, key=lambda x: x['stargazers_count'], reverse=True)[:repo_quantity]
+    return repo_list_sorted
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(repo_list_sorted):
+    for repo in repo_list_sorted:
+        repo_name = repo['full_name']
+        open_issues = requests.get(API_ISSUES_URL.format(repo_name)).json()
+        yield issues
 
 if __name__ == '__main__':
-    pass
+    date_week_earlier = date.today() - timedelta(days=7)
+    repo_list_sorted = get_trending_repositories(date_week_earlier)
+    issues_info = get_open_issues_amount(repo_list_sorted)
+    for issues in issues_info:
+        print(issues)
