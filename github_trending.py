e16496b8a06e721d121d9f944421906665518346
Michail_Z <apxahrejl@gmail.com>
2018-05-27 22:33:18 +0500

    Store

--- github_trending.py
+++ github_trending.py
@@ -1,3 +1,10 @@
+import requests
+
+
+from datetime import date, timedelta
+
+
+API_REPO_URL = 'https://api.github.com/search/repositories?q=created:>{}'
 
 
 def get_trending_repositories(top_size):
@@ -8,4 +15,8 @@ def get_open_issues_amount(repo_owner, repo_name):
     pass
 
 if __name__ == '__main__':
-    pass
+    days_in_week = 7
+    week_ago = date.today() - timedelta(days=days_in_week)
+    search_url = API_REPO_URL.format(week_ago)
+    repo_data = requests.get(search_url).json()
+    print(repo_data)
