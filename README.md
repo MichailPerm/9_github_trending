99aac4dd057df9fe4bf8fb9543606879b32ae8ca
Michail_Z <apxahrejl@gmail.com>
2018-05-28 23:34:09 +0500

    Release of trending git repo parser

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # Github Trends
 
-[TODO. There will be project description]
+This script gets 20 most popular repos and prints opened issues for evetyone of them.
+
+# Requirements
+
+This script need requests version 2.11.1 or more to be installed.
+
+# Quickstart
+
+First - install required libraries:
+```bash
+(lenv) user@desktop:~/lprojects/9_github_trending$ pip install -r requirements.txt 
+```
+
+Second - to start program type in bash:
+```bash
+(lenv) user@desktop:~/lprojects/9_github_trending$ python github_trending.py 
+For repo winjs/winjs got next opened issues:
+        WinJS and in-proc background tasks, url: https://api.github.com/repos/winjs/winjs/issues/1677
+        Angular Support, url: https://api.github.com/repos/winjs/winjs/issues/1676
+        DatePicker is not localized in browser, url: https://api.github.com/repos/winjs/winjs/issues/1675
+        Fluent Design support, url: https://api.github.com/repos/winjs/winjs/issues/1673
+        Stacking grid items with opacity < 1, url: https://api.github.com/repos/winjs/winjs/issues/1671
+        Pivot set PivoteItems render not correct, url: https://api.github.com/repos/winjs/winjs/issues/1670
+        ListView pointerDown/Up animation not work, url: https://api.github.com/repos/winjs/winjs/issues/1669
+        ListView pointerDown/Up animation not work, url: https://api.github.com/repos/winjs/winjs/issues/1668
+        Windows 10 Creators Update broke my ListView, url: https://api.github.com/repos/winjs/winjs/issues/1667
+        ListView items don't support programmatic focus on IE, url: https://api.github.com/repos/winjs/winjs/issues/1666
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -8,25 +8,37 @@ API_ISSUES_URL = 'https://api.github.com/repos/{}/issues'
 
 def get_trending_repositories(date_week_earlier):
     repo_quantity = 20
-    repo_data = requests.get(API_REPO_URL.format(date_week_earlier)).json()
-    repo_list = repo_data['items']
-    repo_list_sorted = sorted(repo_list, key=lambda x: x['stargazers_count'], reverse=True)[:repo_quantity]
-    return repo_list_sorted
+    repo_data_dict = requests.get(
+        API_REPO_URL.format(date_week_earlier)
+    ).json()
+    repo_list = repo_data_dict['items']
+    return sorted(
+        repo_list,
+        key=lambda x: x['stargazers_count'],
+        reverse=True
+    )[:repo_quantity]
 
 
 def get_open_issues_amount(repo_list_sorted):
     for repo in repo_list_sorted:
-        repo_name = repo['full_name']
-        issues = requests.get(API_ISSUES_URL.format(repo_name)).json()
+        issues = requests.get(
+            API_ISSUES_URL.format(repo['full_name'])
+        ).json()
         open_issues = [issue for issue in issues if issue['state'] == 'open']
-        yield {'repo_name': repo_name,
+        yield {'repo_name': repo['full_name'],
                'open_issues': open_issues}
 
+
 if __name__ == '__main__':
-    date_week_earlier = date.today() - timedelta(days=7)
-    repo_list_sorted = get_trending_repositories(date_week_earlier)
-    issues_info = get_open_issues_amount(repo_list_sorted)
-    for issues in issues_info:
-        print('For repo {} got next opened issues:'.format(issues['repo_name']))
-        for open_issue in issues['open_issues']:
-            print('\t{}, url: {}'.format(open_issue['title'], open_issue['url']))
+    date_a_week_ago = date.today() - timedelta(days=7)
+    trending_repo_list = get_trending_repositories(date_a_week_ago)
+    open_issues_amount = get_open_issues_amount(trending_repo_list)
+    for open_issues in open_issues_amount:
+        print('For repo {} got next opened issues:'.format(
+            open_issues['repo_name'])
+        )
+        for open_issue in open_issues['open_issues']:
+            print('\t{}, url: {}'.format(
+                open_issue['title'],
+                open_issue['url'])
+            )
